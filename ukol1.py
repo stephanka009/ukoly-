4b0dacf11a76fa52b5af0852b1b3308a258ad9b9
unknown <stepanka.riegerova@infis.cz>
2025-03-18 11:19:24 +0100

    zkouska

--- ukol1.py
+++ ukol1.py
@@ -2,19 +2,3 @@ jmeno = input("Zadejte sve jmeno: ")
 vek = int(input("Zadejte svuj vek: "))
 
 print(f"ahoj, jmenuji se {jmeno} a je mi {vek} let")
-
-
-cislo1 = float(input("Zadejte prvni desetinne cislo: "))
-cislo2 = float(input("zadejte druhe desetinne cislo: "))
-
-soucet = cislo1 + cislo2
-rozdil = cislo1 - cislo2
-soucin = cislo1 * cislo2
-podil = cislo1 / cislo2 
-if cislo2==0:
-    print("Nelze dělit nulou")
-
-print(f"soucet: {soucet:.2f}")
-print(f"rozdil: {rozdil:.2f}")
-print(f"soucin: {soucin:.2f}")
-print(f"podil: {podil:.2f}"if isinstance(podil, float) else podil)
